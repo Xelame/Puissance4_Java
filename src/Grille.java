78603d1f3244803a9f293afd45c12e097d0608fc
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-09-27 16:30:17 +0200

    Merging Dev Juliette

--- src/Grille.java
+++ src/Grille.java
@@ -1,20 +1,20 @@
 import java.util.ArrayList;
+import java.util.Enumeration;
 
 public class Grille {
 
     public static Grille instance;
-    public static int nombreColonne;
-    public static int nombreLigne;
+    public static int nombreDeColonne;
+    public static int nombreDeLigne;
     public static ArrayList<String> listeDesJoueurs = new ArrayList<String>();
-    private static int nombreDeJoueur = 2;
+    private static int nombreDeJoueur;
+    private static ArrayList<ArrayList<String>> contenu;
 
     private Grille() {
-        ChoosePlayerNumber();
+        nombreDeJoueur = ChoosePlayerNumber();
         // TODO : Réfléchir au système de tour et de joueur
-        listeDesJoueurs.add("O");
-        listeDesJoueurs.add("X");
-        listeDesJoueurs.add("V");
-        System.out.println(GetPlayer(25));
+
+        ToString();
     }
 
     public static Grille getInstance() {
@@ -24,19 +24,48 @@ public class Grille {
         return instance;
     }
 
-    public static String ToString() {
-        // TODO : Retourner sa valeur en type `String` ici pour afficher de la grille :)
-        return "";
+    public static void ToString() {
+        // TODO : Amélioration requise svp :)
+        for(int k = 0; k < nombreDeLigne; k++){
+            System.out.print("#");
+
+            for (int j =0; j< nombreDeColonne; j++){
+                System.out.print(contenu.get(j).get(k));
+            }
+
+            System.out.println("#");
+
+        }
+
+        System.out.println("#".repeat(nombreDeColonne+2));
     }
 
     public static int ChoosePlayerNumber() {
         // TODO : Demander au l'utilisateur combien sont-ils ?
-        return 0;
+        int players = App.promptForInt("Veuillez entrer le nombre de joueurs (2 ou 3)");
+        if (2 <= players && players <= 3) {
+            nombreDeColonne = players * 4;
+            nombreDeLigne = Math.round(players * 3.3f);
+            contenu = GetSizeGrid(players);
+        } else {
+            System.err.println("Please input a valid number");
+            ChoosePlayerNumber();
+        }
+
+        return players;
     }
 
     public static ArrayList<ArrayList<String>> GetSizeGrid(int numberOfPlayer) {
         // TODO : Initialise la taille du Tableau en fonction du nombre de joueur
-        return new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i < nombreDeColonne; i++) {
+            ArrayList<String> arrcolumn = new ArrayList<String>();
+            for (int j = 0; j < nombreDeLigne; j++) {
+                arrcolumn.add(" ");
+            }
+            grid.add(arrcolumn);
+        }
+        return grid;
     }
 
     // Fonction qui donne la lettre que joue un joueur en fonction du tour de jeu
