32839fddc77c311bc644fd1aa5c8fccd19e08bbd
Juliette <juliette.guenard@ynov.com>
2022-09-27 15:37:55 +0200

    premier push : Création class Grid : choosePlayerNumber, initGrid, displayGrid

---
+++ puissance4/src/App.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.Arrays;
+public class App {
+        public static void main(String[] args) throws Exception {
+            Grid.ChoosePlayerNumber();
+        }
+
+    
+        public static String promptForString(String prompt) {
+            System.out.println(prompt);
+            System.out.print("> ");
+            InputStreamReader bts = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(bts);
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                System.err.println("Something went wrong : " + e.getMessage());
+                System.err.println("Please try again.");
+                return promptForString(prompt);
+            }
+        }
+    
+        public static int promptForInt(String prompt) {
+            String response = promptForString(prompt);
+            try {
+                return Integer.parseInt(response);
+            } catch (NumberFormatException e) {
+                System.err.println("Please input a valid number.");
+                return promptForInt(prompt);
+            }
+        }
+}
