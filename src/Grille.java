f417d218c9fc2c2869734ced84a1b725e12f01f5
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-09-27 14:33:44 +0200

    Add some static function

--- src/Grille.java
+++ src/Grille.java
@@ -1,19 +1,31 @@
+package src;
+
 public class Grille {
 
     public static Grille instance;
+    public static int nombreColonne;
+    public static int nombreLigne;
 
     private Grille() {
-
+        ChoosePlayerNumber();
+        // TODO : Réfléchir au système de tour et de joueur
     }
 
-    public static getInstance() {
-        if (instance = null) {
+    public static Grille getInstance() {
+        if (instance == null) {
             instance = new Grille();
         }
         return instance;
     }
 
-    public static void ToString() {
-        // TODO : Afficher de la grille ou retourner sa valeur en type `String` ici :)
+    public static String ToString() {
+        // TODO : Retourner sa valeur en type `String` ici pour afficher de la grille :)
+        return "";
+    }
+
+    public static int ChoosePlayerNumber() {
+        // TODO : Demander au l'utilisateur combien sont-ils ? Et Initialise la taille
+        // du Tableau en conséquence :)
+        return 0;
     }
 }
