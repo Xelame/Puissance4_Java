cf381aa5bd5adc792e0d03ea96fb613376292b65
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-09-27 13:53:23 +0200

    create general function for prompt

---
+++ src/App.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        promptForInt("Hello, how old are you ?");
+    }
+
+    public static String promptForString(String prompt) {
+        System.out.println(prompt);
+        System.out.print("> ");
+        InputStreamReader bts = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(bts);
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            System.err.println("Something went wrong : " + e.getMessage());
+            System.err.println("Please try again.");
+            return promptForString(prompt);
+        }
+    }
+
+    public static int promptForInt(String prompt) {
+        String response = promptForString(prompt);
+        try {
+            return Integer.parseInt(response);
+        } catch (NumberFormatException e) {
+            System.err.println("Please input a valid number.");
+            return promptForInt(prompt);
+        }
+    }
+}
