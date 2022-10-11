e9cb359db18257a84f0a794aa7fd4504f01fefdb
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-11 16:49:34 +0200

    add docs

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        final
         Grille grille = Grille.getInstance();
         grille.Play();
     }
---
+++ src/GameManager.java
@@ -0,0 +1,22 @@
+public class GameManager {
+    // TODO : Mettre ici la création d'une grille
+    public GameManager() {
+        Grille grille = Grille.getInstance();
+    }
+    
+    // TODO : La loop de jeu 
+    public void Play(String Joueurs) {
+        int turnNumber = 0;
+        // TODO : Mettre la condition de fin ici
+        while(true) {
+            
+        }
+    }
+
+
+    // TODO : La Liaison entre les joueurs en réseau
+
+
+    
+    
+}
