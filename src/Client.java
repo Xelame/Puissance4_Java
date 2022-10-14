c9f9ef0801c08d9d9ee64c77f386351c572dd7f9
Mélaine Favennec <melaine.favennec@etu.univ-nantes.fr>
2022-10-14 15:39:38 +0200

    maj

--- src/Client.java
+++ src/Client.java
@@ -1,52 +1,32 @@
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
-<<<<<<< HEAD
 import java.net.ServerSocket;
 import java.util.ArrayList;
-=======
->>>>>>> cb6fe0a7870ab1300f48251e51f2663fe9acb4f3
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.UnsupportedAddressTypeException;
 
 public class Client {
-<<<<<<< HEAD
+    static SocketChannel  clientSocket = null;
+    
     public static void main(String[] args){
+
+
         try{
-            SocketChannel clientSocket = SocketChannel.open();
+             clientSocket = SocketChannel.open();
             clientSocket.connect(new InetSocketAddress("localHost", 8000));
-            /*String message = App.promptForString("choississez une lettre entre de a à g");
-            ByteBuffer  bytes = ByteBuffer.wrap(message.getBytes("UTF-8"));
-            while(bytes.hasRemaining()){
-=======
+             //TODO : wait for other client
+             System.out.println("En attente de joueur la partie va bientôt démarrer ...");
 
-    SocketChannel clientSocket;
-
-    public Client() {
-        try {
-            clientSocket = SocketChannel.open();
-            connectToServer();
-            String message = GameManager.promptForString("choississez une lettre entre de a à g");
-            ByteBuffer bytes = ByteBuffer.wrap(message.getBytes("UTF-8"));
-            while (bytes.hasRemaining()) {
->>>>>>> cb6fe0a7870ab1300f48251e51f2663fe9acb4f3
-                clientSocket.write(bytes);
-                clientSocket.close();
-
+             
+             
                
-            }*/
-            if(Server.nbClient<Server.typeOfgame){
-                System.out.println("en attente d'autres joueurs... ("+Server.getNbClient()+"/"+Server.typeOfgame+")");
-            }
-<<<<<<< HEAD
+            
 
-            //TODO : wait for other client
+           
         }
         catch(IOException e){
-=======
-        } catch (IOException e) {
->>>>>>> cb6fe0a7870ab1300f48251e51f2663fe9acb4f3
             System.err.println(e.toString());
         }
         // en boucle, on peut en attendre une suivante en attendant que quelqu'une
@@ -57,10 +37,12 @@ public class Client {
     public void connectToServer() {
         String choice = GameManager.promptForString("Entrer l'IP du serveur voulu :");
         try {
-            clientSocket.connect(new InetSocketAddress(choice, 4004));
+            clientSocket.connect(new InetSocketAddress(choice, 8000));
         } catch(IOException e) {
             System.out.println("L'adresse entrée est invalide");
             connectToServer();
         }
     }
+
+ 
 }
--- src/Server.java
+++ src/Server.java
@@ -9,37 +9,13 @@ import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 
 public class Server {
-<<<<<<< HEAD
+
     public static int  nbClient =0;
     public static int  typeOfgame =2;
     public static SocketChannel clientSocket;
-    public static ArrayList<SocketChannel> clientSocketList = new ArrayList<SocketChannel>(); //list de client /!\ on peut faire un clientSocket.getLocalAddress() !!
-
-    public static void main(String[] args){
+    
    
-        try{
-        ServerSocketChannel serverSocket = ServerSocketChannel.open();
-        serverSocket.bind(new InetSocketAddress(8000));
-        while(true){
-           
-            while(nbClient<typeOfgame){
-            //TODO : accepter 2 ou 3 client mais pas plus
 
-            // TODO :  verif que en reseau il n'y ait pas deux clients du même ordi??
-            nbClient = clientSocketList.size();
-            System.out.println("nbclient = "+ nbClient);
-            SocketChannel clientSocket = serverSocket.accept();
-            System.out.println(" un client s'est connecté = " +clientSocket+" | END adress : "+ clientSocket.getLocalAddress());
-            clientSocketList.add(clientSocket);
-
-           
-            //Listen(clientSocket);
-            //clientSocket.close();
-            }
-        }
-        }
-        catch(IOException e){
-=======
     public Server() {
         getMyIpAddress();
         Run();
@@ -51,17 +27,29 @@ public class Server {
         try {
             int numberOfClient = 0;
             ArrayList<SocketChannel> list = new ArrayList<SocketChannel>();
+            ArrayList<Client> listClient = new ArrayList<Client>();
             ServerSocketChannel serverSocket = ServerSocketChannel.open();
-            serverSocket.bind(new InetSocketAddress(4004));
+            serverSocket.bind(new InetSocketAddress(8000));
             System.out.println("En attente de joueur ...");
             while (numberOfClient < GameManager.numberOfPlayer) {
                 numberOfClient++;
+                Client client = new Client();
                 SocketChannel clientSocket = serverSocket.accept();
                 System.out.println("Un client s'est connecté\nTotal : " + numberOfClient + " / " + GameManager.numberOfPlayer);
                 list.add(clientSocket);
+                listClient.add(client);
+               
             }
+
+            //TODO : envoi signal la partie démarre
+            for (int i=0; i < list.size(); i++){
+                //listClient.get(i).Ready();
+            }
+
+
+         
+
         } catch (IOException e) {
->>>>>>> cb6fe0a7870ab1300f48251e51f2663fe9acb4f3
             System.err.println(e.toString());
         }
     }
@@ -77,10 +65,6 @@ public class Server {
         System.out.println(message);
     }
 
-<<<<<<< HEAD
-    public static int getNbClient(){
-        return nbClient;
-=======
     public void getMyIpAddress() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
@@ -89,6 +73,5 @@ public class Server {
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
->>>>>>> cb6fe0a7870ab1300f48251e51f2663fe9acb4f3
     }
 }
