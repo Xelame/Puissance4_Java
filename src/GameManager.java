1ad653614d3de46d5b12b5707f1ab96198c04bb3
AROLLAND1 <alexandre.rolland85@ynov.com>
2022-10-14 10:03:22 +0200

    add GameManager

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,18 +1,39 @@
 public class GameManager {
     // TODO : Mettre ici la création d'une grille
+    Grille grille = Grille.getInstance();
+
     public GameManager() {
-        Grille grille = Grille.getInstance();
     }
     
-    // TODO : La loop de jeu 
-    public void Play(String Joueurs) {
+    // TODO : La loop de jeu ✅
+    public void Play() {
         int turnNumber = 0;
+        Boolean isRunning = true;
         // TODO : Mettre la condition de fin ici
-        while(true) {
-            
+        while (isRunning) {
+            // TODO : Faire l'interaction voulu :)
+            String laLettreQueNousDonneLeJoueur = grille.chooseColumn(turnNumber);
+            grille.fillColumn(laLettreQueNousDonneLeJoueur, turnNumber);
+            if (grille.isRunning(grille.getPlayerLetter(turnNumber))) {
+                isRunning = !isRunning;
+            }
+            turnNumber++;
         }
     }
 
+    public void chooseCommunication() {
+        int choice = App.promptForInt("Puissance 4 - Multiplayer \n1 - Local\n2 - Reseau");
+        switch (choice) {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            default:
+
+                break;
+        }
+    }
 
     // TODO : La Liaison entre les joueurs en réseau
 
